413b0511a9f03ea90ee95014e57dcb5636a5c4a9
YOUNGGYU JEONG <zkdlf90@gmail.com>
2023-02-13 21:39:38 +0900

    깃, AWS, 데이터베이스 설정

---
+++ src/main/java/com/korit/younggyu/YounggyuApplication.java
@@ -0,0 +1,13 @@
+package com.korit.younggyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounggyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YounggyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korit/younggyu/YounggyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.younggyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YounggyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
